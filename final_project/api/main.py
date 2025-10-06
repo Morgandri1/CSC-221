88c8d1137b9f47a96aed50d4dc0a864603529b8f
Morgan Metz <morgan.metz@eyekon.xyz>
2025-10-06 11:20:58 -0400

    update final project api

--- final_project/api/main.py
+++ final_project/api/main.py
@@ -1,12 +1,19 @@
-from quart import Quart, send_from_directory, jsonify, request, abort
+from quart import Quart, send_file, jsonify, request, abort
 from quart_cors import cors
 from werkzeug.exceptions import HTTPException, InternalServerError
 import os
+from solders.pubkey import Pubkey
 
 app = Quart("CSC")
 app = cors(app, allow_origin="*", allow_methods=["GET", "POST"])
 app.config['UPLOAD_FOLDER'] = "/s3"
 APP_SECRET = os.environ.get("APP_SECRET")
+
+def generate_uuid(filename: str) -> str:
+    # generates an ed25519 compliant pubkey which we'll use as our file's uuid
+    uuid = Pubkey.new_unique()
+    filetype = filename.split(".")[-1] # get the last slice of the split filename to get suffix
+    return str(uuid) + "." + filetype
 
 @app.before_request
 async def authenticate_platform():
@@ -21,14 +28,14 @@ async def authenticate_platform():
         
 @app.route("/get/<path:path>", methods=["GET"])
 async def cdn(path: str):
-    return await send_from_directory("/s3", path)
+    return await send_file("/s3/"+path, )
     
 @app.route("/upload", methods=["POST"])
 async def push():
     try:
         file = await request.files
         file = file["file"]
-        name = file.filename
+        name = generate_uuid(file.filename)
         await file.save(f"/s3/{name}")
         return jsonify({"status": 0, "filename": name})
     except Exception as e:
