9a9d34c36bcc9b6373c04b48a1b52b4d9fab344b
Morgan Metz <morgan.metz@eyekon.xyz>
2025-10-30 10:22:36 -0400

    document

--- final_project/api/main.py
+++ final_project/api/main.py
@@ -9,36 +9,38 @@ app.config['UPLOAD_FOLDER'] = "/s3"
 APP_SECRET = os.environ.get("APP_SECRET")
 
 def generate_uuid(filename: str) -> str:
-    # generates an ed25519 compliant pubkey which we'll use as our file's uuid
+    # generates an ed25519 compliant pubkey which we'll use as file's uuid
     uuid = Pubkey.new_unique()
-    filetype = filename.split(".")[-1] # get the last slice of the split filename to get suffix
+    # get the last slice of the split filename to get suffix
+    filetype = filename.split(".")[-1]
     return str(uuid) + "." + filetype
 
 @app.before_request
 async def authenticate_platform():
     if request.method == "OPTIONS" or request.path.startswith("/get"):
-        return
+        return # allow OPTIONS requests and GET requests without authorization
     headers = request.headers
-    secret = headers.get("Authorization")
+    secret = headers.get("Authorization") # get app secret header 
     if not secret or not APP_SECRET or secret != APP_SECRET:
         abort(401, "Unauthorized")
     else:
         return
         
 @app.route("/get/<path:path>", methods=["GET"])
-async def cdn(path: str):
-    return await send_file("/s3/"+path, )
+async def get(path: str):
+    # send file helper will load file payload and construct response 
+    return await send_file("/s3/"+path) 
     
 @app.route("/upload", methods=["POST"])
-async def push():
+async def upload():
     try:
         file = (await request.files).get("file")
         if file is None:
             abort(400, "No file provided")
         name = generate_uuid(file.filename)
-        await file.save(f"/s3/{name}")
+        await file.save(f"/s3/{name}") # save to s3 folder with uuid
         return jsonify({"status": 0, "filename": name})
     except Exception as e:
         return jsonify({"status": 1, "error": str(e)})        
 
-app.run("0.0.0.0", port=80)
+app.run("0.0.0.0", port=80) # run on default http port - forwarded to https via host 
